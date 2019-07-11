897bcc32a9fb58236994dab8cefc6b6a280caf54
guilherme henrique <ghsmmvts@gmail.com>
2019-07-11 12:01:57 -0300

    Adicionar conteúdo

--- script03.js
+++ script03.js
@@ -37,10 +37,19 @@
 // let material =  prompt = ('qual é o material');
 
 let lancesDeEscada = Number (prompt ('Quantos lances de escada você gostaria'))
+
+while(isNaN(lancesDeEscada)){
+    lancesDeEscada = Number (prompt (`Você digitou "${lancesDeEscada}. O programa só aceita valores numéricos nessa etapa. Por favor, digite quantos lances de escada você gostaria.`))
+}
+
 let material = prompt ('Qual o material da sua escada');
 let desenho= '';
 
 let i = 0;
+
+// while(isNaN(lancesDeEscada)){
+//     lancesDeEscada = Number (prompt (`Você digitou "${lancesDeEscada}. O programa só aceita valores numéricos nessa etapa. Por favor, digite quantos lances de escada você gostaria.`))
+// }
 
 while (i< lancesDeEscada){
     desenho = desenho + material;
