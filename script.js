ceda7cf28b382d68e63a6fd5576d67af4bbd67f9
guilherme henrique <ghsmmvts@gmail.com>
2019-07-11 10:13:11 -0300

    adicionar primeiros arquivos

---
+++ script.js
@@ -0,0 +1,38 @@
+// console.log('Hello World');
+
+// let nomeDoUsuario = 'Guilherme';
+// let nomeDoUsuario = prompt('Qual é o seu nome');
+// // let idade = 23;
+// let idade = prompt("Quantos anos você tem");
+// // let vivo = false;
+// let programador = confirm('Você é um programador');
+
+// console.log('Olá' + nomeDoUsuario);
+// alert('Olá' +  nomeDoUsuario);
+// alert('Olá' +  nomeDoUsuario + ', você tem' + idade + ' anos.');
+// console.log(idade);
+// alert(idade);
+// console.log(vivo);
+// alert(vivo)
+
+// // if (programador === true){
+//     if(programador){
+//         alert('Você é um programador, parabéns');
+//     }
+//     else if(Number(idade) >= 16 && Number(idade) <= 25){
+//         alert('Que pena, você já ouviu falar da estação hack');
+//     }
+//     else{
+//         alert('Que pena, você deveria arpender JavaScrpit');
+//     }
+
+// console.log('Trocando o nome do usuário pra Bob');
+// nomeDoUsuario = 'Bob';
+// console.log(nomeDoUsuario);
+
+// TABELA VERDADE:
+// && (AND)
+// true && true = true
+// true && false = false
+// false && false = false
+
---
+++ script01.js
@@ -0,0 +1,40 @@
+// Nós vamos criar um sistema que avalia se o aluno foi aprovado ou não em uma matéria. Pergunte pro usuário:
+
+// -nome do aluno
+// a matéria
+// a Nota
+// Com os valores recebidos, verifique se o aluno teve nota superior a média (6). A saída do programa deve falar:
+// parabéns, o aluno x foi aprovado na materi 
+
+// console.log('Sistema');
+// let nome = prompt('Qual seu nome');
+// let matéria = prompt ('Qual é a matéria');
+// let nota = prompt ('Qual sua nota');
+
+// if(nome){
+//     alert ('Parabéns, o ' + nome + 'foi aprovado na máteria' + nota);
+// else if(nome){}
+
+// else(){}
+
+let aluno = prompt ('qual seu nome');
+let matéria = prompt ('Qual sua matéria')
+let nota = prompt ('Qual foi sua nota');
+
+let media = 6;
+
+if (Number(nota) >= media) {
+    // alert ('Parabéns, o aluno' + aluno + 'foi aprovado na máteria' + matéria);
+// Template String
+    alert(`Parabéns, o aluno ${aluno} foi aprovado na matéria ${matéria}`);
+}
+else if (Number(nota) >= media - 0.5){
+    // alert ('O aluno' + aluno + 'foi aprovado na máteria' +  matéria + 'pelo sistema');
+    // Template String
+    alert(`O aluno ${aluno} foi aprovado na matéria ${matéria} pelo sistema`);
+}
+else{
+    // alert('Parece que a nota do aluno' + aluno + 'na matéria' + matéria + ' foi insuficiente');
+    // Template String
+    alert(`Parace que a nota do aluno ${aluno} na matéria ${matéria} foi insuficiente`);
+}
